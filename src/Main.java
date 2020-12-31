f8a4e11f21924c99f3026774c9c51742580f3864
karol <karol.jarosinski91@gmail.com>
2020-12-31 16:17:01 +0100

    Poprawka

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Siema");
-        System.out.println("Siema");
+        System.out.println("Siema12345");
     }
 }
